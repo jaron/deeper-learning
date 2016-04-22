41e8d8ceed9bdb1b314fcf83606e57e7ab5e0984
Jaron <mail@jaroncollis.com>
2016-04-22 16:29:18 +0100

    added learning curve plot

--- regression/visualise.py
+++ regression/visualise.py
@@ -2,6 +2,43 @@ import matplotlib.pyplot as pl
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.learning_curve import learning_curve
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+    from http://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
 
 
 
@@ -64,7 +101,7 @@ def model_complexity(X_train, y_train, X_test, y_test):
     """ Calculates the performance of the model as model complexity increases.
         The learning and testing errors rates are then plotted. """
 
-    print "Creating a model complexity graph. . . "
+    print "Creating a model complexity graph for decision trees of depth 1 to 14"
 
     # We will vary the max_depth of a decision tree model from 1 to 14
     max_depth = np.arange(1, 14)
